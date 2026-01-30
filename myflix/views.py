e83c05e31cc3b7983d6d9ddbf168b526bd20221e
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-01-30 19:03:13 -0300

    Aula 11 – Codificando a API com Django Rest Framework

--- myflix/views.py
+++ myflix/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def users(request):
+    if request.method == 'GET':
+        user = {
+            'id': 1,
+            'name': 'José'
+        }
+
+    return JsonResponse(user, json_dumps_params={'ensure_ascii': False})
