0a6aa147899dfd3fa60fe3c6f7c924437b3794b7
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-01-31 12:29:48 -0300

    Aula 14 – Models

--- myflix/admin.py
+++ myflix/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from myflix.models import User, Stream
 
-# Register your models here.
+class Users(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'cpf', 'birthday', 'cell_phone')
+    list_display_links = ('id', 'name')
+    list_per_page = 20
+    search_fields = ('name',)
+
+admin.site.register(User, Users)
+
+class Streams(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description')
+    list_display_links = ('id', 'code')
+    search_fields = ('code', 'description')
+
+admin.site.register(Stream, Streams)
