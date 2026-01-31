2c45e6d0cd052c9428816c136ad495d1ff6a7c59
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-01-31 12:10:07 -0300

    Aulas 12 e 13 - Criação das Models User e Stream

--- myflix/models.py
+++ myflix/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=False, max_length=50)
+    cpf = models.CharField(max_length=11)
+    birthday = models.DateField()
+    cell_phone = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.name
+
+
+class Stream(models.Model):
+    CATEGORIES = (
+        ('F', 'Film'),
+        ('S', 'Serie'),
+        ('D', 'Documentary')
+    )
+    code = models.CharField(max_length=10)
+    description = models.CharField(blank=False, max_length=100)
+    category = models.CharField(blank=False, choices=CATEGORIES, default='F')
+
+    def __str__(self):
+        return self.code
